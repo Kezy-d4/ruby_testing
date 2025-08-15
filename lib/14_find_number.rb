b71bbfbfa468ae32708c6bf3fa25719514188d3d
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-08-15 19:29:18 -0400

    exercise: 14_find_number

--- lib/14_find_number.rb
+++ lib/14_find_number.rb
@@ -2,7 +2,8 @@
 
 # class for computer to find random number
 class FindNumber
-  attr_reader :min, :max, :answer, :guess
+  attr_reader :answer, :guess
+  attr_accessor :min, :max
 
   def initialize(min, max, answer = RandomNumber.new(min, max), guess = nil)
     @min = min
@@ -10,5 +11,18 @@ class FindNumber
     # The RandomNumber class will have an instance variable '@value'
     @answer = answer.value
     @guess = guess
+  end
+
+  def make_guess
+    average = (min + max) / 2
+    average.floor
+  end
+
+  def game_over?
+    guess == answer
+  end
+
+  def update_range
+    guess < answer ? self.min = guess + 1 : self.max = guess - 1
   end
 end
